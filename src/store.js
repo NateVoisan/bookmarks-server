82e0ac7729ac08a4e82ad04e1dfee28ddfc307b2
Nate Voisan <rufio96@gmail.com>
2020-10-30 09:08:19 -0700

    database commit

--- src/store.js
+++ src/store.js
@@ -1,27 +1,21 @@
-const { v4: uuidv4 } = require('uuid');
+const uuid = require('uuid/v4')
 
 const bookmarks = [
-    {
-        id: uuidv4(),
-        title: 'Thinkful',
-        url: 'https://www.thinkful.com',
-        description: 'Think outside the classroom',
-        rating: 5
-    },
-    {
-        id: uuidv4(),
-        title: 'Google',
-        url: 'https://www.google.com',
-        description: 'Where we find everything else',
-        rating: 4
-    },
-    {
-        id: uuidv4(),
-        title: 'MDN',
-        url: 'https://developer.mozilla.org',
-        description: 'The only place to find web documentation',
-        rating: 5
-    }
+  { id: uuid(),
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: uuid(),
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+  { id: uuid(),
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5 },
 ]
 
-module.exports = { bookmarks };
+module.exports = { bookmarks }
